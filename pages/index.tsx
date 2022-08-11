4eac5b7c2f46fc2d6444411dd1a3c89e7f166063
FUJIKAWA-IKU <fujikawaiku.potepan@gmail.com>
2022-08-11 15:12:55 +0900

    お試し2

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!ステージング</a>
+          Welcome to <a href="https://nextjs.org">Next.js!ステージングわわ</a>
         </h1>
 
         <p className={styles.description}>
